b168da96ef68a272d014b39e6cdd79861ee610c0
Tigon-444 <WTigon-444@Yandex.ru>
2023-07-16 17:00:26 +0300

    Добавьте файлы проекта.

---
+++ CatalogPlanet.cs
@@ -0,0 +1,30 @@
+﻿namespace DZ6._2
+{
+    internal class CatalogPlanet
+    {       
+        private List<Planet> catalogPlanet = new List<Planet>();
+        private int counter;
+
+        public CatalogPlanet()
+        {
+            catalogPlanet.Add(new Planet("Venera"));
+            catalogPlanet.Add(new Planet("Earth"));
+            catalogPlanet.Add(new Planet("Mars"));
+        }
+
+
+        public (int? numberFromSun, int? lengthEquator, string? error) GetPlanet(string name)
+        {
+            var objectPlanet = catalogPlanet.Find(planet => planet.name == name);
+            counter++;
+            if (counter == 3)
+            {
+                counter = 0;
+                return (null, null, "Вы спрашиваете слишком часто");
+            }
+            else if (objectPlanet == null) return (null, null, "Не удалось найти планету " + name);
+            
+            return (objectPlanet.numberFromSun, objectPlanet.lengthEquator, null);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace DZ6._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new CatalogPlanet();
+            WritePlanet(a.GetPlanet("Earth"), "Earth");
+            WritePlanet(a.GetPlanet("Limonia"), "Limonia");
+            WritePlanet(a.GetPlanet("Mars"), "Mars");
+
+        }
+
+        static void WritePlanet((int?, int?, string?) info, string name)
+        {
+            if (info.Item3 == null)
+            {
+                Console.WriteLine("Планета " + name);
+                Console.WriteLine("Номер от Солнца " + info.Item1);
+                Console.WriteLine("Длинна экватора " + info.Item2);
+            }
+            else
+            {
+                Console.WriteLine(info.Item3);
+            }
+            Console.WriteLine();
+        }
+    }
+
+}
